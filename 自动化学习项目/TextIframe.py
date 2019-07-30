2f7bf0fbee8751be6e20497ffe485797c89dc336
liyinlin77 <liyinlin77@163.com>
2019-07-30 13:47:05 +0800

    aa

--- 自动化学习项目/TextIframe.py
+++ 自动化学习项目/TextIframe.py
@@ -7,13 +7,22 @@ def Text10():
     driver.get("https://mail.163.com/")
     driver.maximize_window()
     time.sleep(5)
+    driver.find_element_by_id("lbNormal").click()
     iframe = driver.find_element_by_xpath("//iframe[starts-with(@id, 'x-URS-iframe')]")
     # driver_iframe =
     driver.switch_to.frame(iframe)
     # //*[@id='login-form']
     # driver.find_element_by_xpath("/html/body/div[2]/div[2]//from/div/div[1]/div[2]/input").send_keys("1600000000")
-    driver.find_element_by_xpath("//*[@id='login-form']/div/div[1]/div[2]/input").send_keys("1600000000")
-    time.sleep(20)
+    driver.find_element_by_xpath("//*[@id='login-form']/div/div[1]/div[2]/input").send_keys("liyinlin77")
+    driver.find_element_by_xpath("//*[@id='login-form']/div/div[3]/div[2]/input[2]").send_keys("pandannicc.")
+    # driver.find_element_by_css_selector("[type='password']").send_keys("liyinlin77.")
+    driver.find_element_by_id("dologin").click()
+    # driver.switch_to.default_content()
+    time.sleep(3)
+    # 断言
+    assert "liyinlin77@163.com" in driver.page_source, u"登录失败"
+    time.sleep(2)
+    driver.quit()
 
 
 if __name__ == "__main__":
