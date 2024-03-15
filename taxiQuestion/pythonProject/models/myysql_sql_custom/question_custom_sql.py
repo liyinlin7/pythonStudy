7d1a7a917b2c82a02e7133070cba1a9fef015167
liyinlin <yinlin.li@ftimage.cn>
2024-03-15 16:49:54 +0800

    taxi-sql

--- taxiQuestion/pythonProject/models/myysql_sql_custom/question_custom_sql.py
+++ taxiQuestion/pythonProject/models/myysql_sql_custom/question_custom_sql.py
@@ -4,6 +4,7 @@ from sqlalchemy import and_
 from models.mysql_resule import result_all_one, result_leftjoin_all_many
 import logging
 from common import my_logger
+from sqlalchemy import text
 
 class QuestionCustomSql(object):
 
@@ -11,18 +12,39 @@ class QuestionCustomSql(object):
         self.engine = engine
         self.session = session
 
-    def question_select_and(self, question_id:list=None, question_type:list=None):
-        for var_name in ['question_id', 'question_type']:
-            var_value = locals()[var_name]
-            if var_value is not None:
-                self.question_conditions[var_name] = var_value
-        query = self.session.query( Questions )
-        for field, value in self.question_conditions.items():
-            if value is not None:  # 如果有值，添加为搜索条件
-                query = query.filter( getattr( Questions, field ).in_(value) )
-        result = query.all()
-        __result = result_all_one(result)
-        print(__result)
+    # def question_select_custom_and(self, question_id=['1','2'], question_type:list=['1','2']):
+    def question_select_custom_and(self, question_id=None, question_type:list=None):
+        conditions = []
+        params = {}
+        # 添加条件到查询中
+        if question_id is not None:
+            # sql += " where question_id = :question_id"
+            # params['question_id'] = question_id
+            placeholders_id = ', '.join( [':id' + str( i ) for i in range( len( question_id ) )] )
+            conditions.append(" question_id IN (" + placeholders_id  + ")")
+            # 为每个占位符添加对应的问题类型
+            for i, id in enumerate( question_id ):
+                params['id' + str( i )] = id
+        if question_type is not None:
+            # sql += " and question_type = :question_type"
+            # params['question_type'] = question_type
+            placeholders_type = ', '.join( [':type' + str( i ) for i in range( len( question_type ) )] )
+            conditions.append(" question_type IN (" + placeholders_type + ")")
+            # 为每个占位符添加对应的问题类型
+            for i, type in enumerate( question_type ):
+                params['type' + str( i )] = type
+        # 构建SQL查询
+        sql = "SELECT * FROM questions"
+        if conditions:
+            sql += " WHERE " + " AND ".join( conditions )
+        # 执行查询
+        with session.begin():
+            result = session.execute( text( sql ), params )
+            rows = result.fetchall()
+            keys = result.keys()
+        # 将结果转换为字典列表并打印
+        result_dict = [dict(zip(keys, row)) for row in rows]
+        print( result_dict )
         # questions_as_dict = [
         #     {
         #         'question_id': question.question_id,
@@ -37,33 +59,45 @@ class QuestionCustomSql(object):
         # print( questions_as_dict )
         # return questions_as_dict
 
-    def question_leftjoin_userAnswer_and(self, question_id:list=None, question_type:list=None, user_answer:list=None):
-        for var_name in ['question_id', 'question_type']:
-            var_value = locals()[var_name]
-            if var_value is not None:
-                self.question_conditions[var_name] = var_value
-        for var_name in ['user_answer']:
-            var_value = locals()[var_name]
-            if var_value is not None:
-                self.user_answer_conditions[var_name] = var_value
-        query = (self.session.query( Questions, UserAnswer )
-                 .outerjoin( UserAnswer, and_(Questions.question_id == UserAnswer.question_id )))
-        # 添加 Questions 的搜索条件
-        for field, value in self.question_conditions.items():
-            if value is not None:  # 如果有值，添加为搜索条件
-                query = query.filter( getattr( Questions, field ).in_(value) )
-        # 添加 UserAnswer 的搜索条件
-        for field, value in self.user_answer_conditions.items():
-            if value is not None:  # 如果有值，添加为搜索条件
-                query = query.filter( getattr( UserAnswer, field ).in_(value) )
-        result = query.all()
-        fields_dict = {}
-        table_name = []
-        fields_dict['useranswer'] = UserAnswer.useranswer_key
-        table_name.append(Questions.__tablename__)
-        table_name.append(UserAnswer.__tablename__)
-        __result = result_leftjoin_all_many( result, fields_dict, table_name )
-        print( __result )
+    def question_leftjoin_userAnswer_coustom_and(self, question_id:list=None, question_type:list=None, user_answer:list=None):
+    # def question_leftjoin_userAnswer_coustom_and(self, question_id=['1','2'], question_type:list=['2','3'],  user_answer:list=None):
+        conditions = []
+        params = {}
+        # 添加条件到查询中
+        if question_id is not None:
+            # sql += " where question_id = :question_id"
+            # params['question_id'] = question_id
+            placeholders_id = ', '.join( [':id' + str( i ) for i in range( len( question_id ) )] )
+            conditions.append( " questions.question_id IN (" + placeholders_id + ")" )
+            # 为每个占位符添加对应的问题类型
+            for i, id in enumerate( question_id ):
+                params['id' + str( i )] = id
+        if question_type is not None:
+            # sql += " and question_type = :question_type"
+            # params['question_type'] = question_type
+            placeholders_type = ', '.join( [':type' + str( i ) for i in range( len( question_type ) )] )
+            conditions.append( " questions.question_type IN (" + placeholders_type + ")" )
+            # 为每个占位符添加对应的问题类型
+            for i, type in enumerate( question_type ):
+                params['type' + str( i )] = type
+        if user_answer is not None:
+            # sql += " where question_id = :question_id"
+            # params['question_id'] = question_id
+            placeholders_user_answer = ', '.join( [':username' + str( i ) for i in range( len( user_answer ) )] )
+            conditions.append( " useranswer.user_answer IN (" + placeholders_user_answer + ")" )
+            # 为每个占位符添加对应的问题类型
+            for i, username in enumerate( user_answer ):
+                params['username' + str( i )] = username
+        sql = "SELECT * FROM questions left join useranswer on questions.question_id = useranswer.question_id"
+        if conditions:
+            sql += " WHERE " + " AND ".join( conditions )
+        # 执行查询
+        with session.begin():
+            result = session.execute( text( sql ), params )
+            rows = result.fetchall()
+            keys = result.keys()
+        result_dict = [dict( zip( keys, row ) ) for row in rows]
+        print( result_dict )
 
 
 if __name__ == '__main__':
@@ -71,6 +105,6 @@ if __name__ == '__main__':
     my_sql_driver = MySqlDriver()
     engine, session = my_sql_driver.connect_mysql()
     engine, session = my_sql_driver.mysql_repeat(engine, session)
-    question_sql = QuestionSql(engine, session)
-    # question_sql.question_select_and()
-    question_sql.question_leftjoin_userAnswer_and()
+    question_sql = QuestionCustomSql(engine, session)
+    # question_sql.question_select_custom_and()
+    question_sql.question_leftjoin_userAnswer_coustom_and()
