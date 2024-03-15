76d418a2f37d21e06787e690399ff5cf224379cc
liyinlin <yinlin.li@ftimage.cn>
2024-03-15 11:51:59 +0800

    taix-sql

--- taxiQuestion/pythonProject/models/mysql_sql_model/question_sql.py
+++ taxiQuestion/pythonProject/models/mysql_sql_model/question_sql.py
@@ -10,15 +10,22 @@ class QuestionSql(object):
     def __init__(self, engine, session):
         self.engine = engine
         self.session = session
-
-
-    def question_select(self, question_id:list=None, question_type:list=None):
-        search_conditions = {
-            'question_id': question_id,
-            'question_type': question_type
+        self.question_conditions = {
+            'question_id': None,
+            'question_type': None
         }
+        self.user_answer_conditions = {
+            'user_answer': None,
+            'answer_bool': None
+        }
+
+    def question_select_and(self, question_id:list=None, question_type:list=None):
+        for var_name in ['question_id', 'question_type']:
+            var_value = locals()[var_name]
+            if var_value is not None:
+                self.question_conditions[var_name] = var_value
         query = self.session.query( Questions )
-        for field, value in search_conditions.items():
+        for field, value in self.question_conditions.items():
             if value is not None:  # 如果有值，添加为搜索条件
                 query = query.filter( getattr( Questions, field ).in_(value) )
         result = query.all()
@@ -33,14 +40,30 @@ class QuestionSql(object):
         #         'question_type': question.question_type,
         #         'range': question.range,
         #         'type': question.type,
-        #     } for question in questions
+        #     } for question in result
         # ]
         # print( questions_as_dict )
         # return questions_as_dict
 
-    def question_leftjoin_userAnswer(self):
-        query = self.session.query( Questions, UserAnswer ).outerjoin( UserAnswer, and_(Questions.question_id == UserAnswer.question_id ))
-        # query = self.session.query( Questions ).options( joinedload( Questions.useranswer ) ).all()
+    def question_leftjoin_userAnswer_and(self, question_id:list=None, question_type:list=None, user_answer:list=None):
+        for var_name in ['question_id', 'question_type']:
+            var_value = locals()[var_name]
+            if var_value is not None:
+                self.question_conditions[var_name] = var_value
+        for var_name in ['user_answer']:
+            var_value = locals()[var_name]
+            if var_value is not None:
+                self.user_answer_conditions[var_name] = var_value
+        query = (self.session.query( Questions, UserAnswer )
+                 .outerjoin( UserAnswer, and_(Questions.question_id == UserAnswer.question_id )))
+        # 添加 Questions 的搜索条件
+        for field, value in self.question_conditions.items():
+            if value is not None:  # 如果有值，添加为搜索条件
+                query = query.filter( getattr( Questions, field ).in_(value) )
+        # 添加 UserAnswer 的搜索条件
+        for field, value in self.user_answer_conditions.items():
+            if value is not None:  # 如果有值，添加为搜索条件
+                query = query.filter( getattr( UserAnswer, field ).in_(value) )
         result = query.all()
         fields_dict = {}
         table_name = []
@@ -57,5 +80,5 @@ if __name__ == '__main__':
     engine, session = my_sql_driver.connect_mysql()
     engine, session = my_sql_driver.mysql_repeat(engine, session)
     question_sql = QuestionSql(engine, session)
-    question_sql.question_select()
-    # question_sql.question_leftjoin_userAnswer()
+    # question_sql.question_select_and()
+    question_sql.question_leftjoin_userAnswer_and()
